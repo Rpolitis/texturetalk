da2294ee1a3924ab13d46c2d328a29d2cce69a51
Rpolitis <Ryan_Politis@student.uml.edu>
2024-03-12 17:06:17 -0400

    v0.0.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import About from "./pages/about";
+// import Contact from "./pages/contact";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          welcome to
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          texturetalk.
+        </h1>
+        {/* <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route path="/about" element={<About />}/>
+                <Route path="/contact" element={<Contact />}/>
+              </Route>
+            </Routes>
+        </BrowserRouter> */}
       </header>
     </div>
   );
