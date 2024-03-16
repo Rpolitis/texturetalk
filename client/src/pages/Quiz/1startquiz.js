97a8c78ef8554d2039a6cb3c5498db701a552497
aririb <arianapb03@gmail.com>
2024-03-16 18:26:00 -0400

    badbutbetter

--- client/src/pages/Quiz/1startquiz.js
+++ client/src/pages/Quiz/1startquiz.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { useHistory } from 'react-router-dom';
+
+const StartQuiz = () => {
+    const history = useHistory();
+
+    const handleStartQuiz = () => {
+        history.push('/2type.js'); // redirect to q1
+    };
+};
 
 const Quiz = () => {
     return (
-        <div>
-            <h1>ready to find your perfect hair routine?</h1>
-            <h3>take our quick quiz for your personalized match.</h3>
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Ready to find your perfect hair routine?</h1>
+            <h3>Take our quick quiz for your personalized match.</h3>
+            <button className="start-button" onClick={handleStartQuiz}>
+                start the quiz
+            </button>
         </div>
     );
 };
