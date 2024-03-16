c66313c6cd0f009d6867f78a444d0c930be0280c
Rpolitis <Ryan_Politis@student.uml.edu>
2024-03-16 17:02:12 -0400

    v0.0.3

--- client/src/pages/Privacy/privacy.js
+++ client/src/pages/Privacy/privacy.js
@@ -1,9 +1,48 @@
 import React from "react";
+import './privacy.css';
 
 const Privacy = () => {
     return (
-        <div>
-            <h1>Hello privacy!</h1>
+        // Privacy Policy Template from: https://www.websitepolicies.com/blog/sample-privacy-policy-template
+        <div className="privacy">
+            <h1>PRIVACY POLICY</h1>
+            <p>This privacy policy ("policy") will help you understand how Texturetalk ("us", "we", "our") uses and protects the data you provide to us when you visit and use Texturetalk ("website ", "service").</p>
+            <p>We reserve the right to change this policy at any given time, of which you will be promptly updated. If you want to make sure that you are up to date with the latest changes, we advise you to frequently visit this page.</p>
+            <h2>What User Data We Collect</h2>
+            <p>When you visit the website, we may collect the following data:</p>
+            <ul>
+                <li>
+                    <p>Your IP address.</p>
+                </li>
+                <li>
+                    <p>Your contact information and email address.</p>
+                </li>
+                <li>
+                    <p>Data profile regarding your online behavior on our website.</p>
+                </li>
+            </ul>
+            <h2>Why We Collect Your Data</h2>
+            <p>We are collecting your data for several reasons:</p>
+            <ul>
+                <li>
+                    <p>To better understand your needs.</p>
+                </li>
+                <li>
+                    <p>To improve our service.</p>
+                </li>
+                <li>
+                    <p>To customize our website according to your online behavior and personal preferences.</p>
+                </li>
+            </ul>
+            <h2>Safeguarding and Securing the Data</h2>
+            <p>Texturetalk is committed to securing your data and keeping it confidential.</p>
+            <p>Texturetalk has done all in its power to prevent data theft, unauthorized access, and disclosure by implementing the latest technologies and software, which help us safeguard all the information we collect online.</p>
+            <h2>Links to Other Websites</h2>
+            <p>Our website may contains links that lead to other websites. If you click on these links Texturetalk is not held responsible for your data and privacy protection. Visiting those websites is not governed by this privacy policy agreement. Make sure to read the privacy policy documentation of the website you go to from our website.</p>
+            <h2>Restricting the Collection of your Personal Data</h2>
+            <p>At some point, you might wish to restrict the use and collection of your personal data. If you have already agreed to share your information with us, feel free to contact us via email and we will be more than happy to delete your information.</p>
+            <p>Texturetalk will not lease, sell or distribute your personal information to any third parties, unless we have your permission. We might do so if the law forces us.</p>
+            <p>By visiting this page, you agree to our policy</p>
         </div>
     );
 };
