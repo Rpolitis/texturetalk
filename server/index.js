ed42062cfa528a9c63574db17f802d048bd7b36b
dsun6001 <dsun6001@gmail.com>
2024-03-16 20:13:52 -0400

    replace package.json and package-lock.json with colby's version

--- server/index.js
+++ server/index.js
@@ -19,8 +19,7 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://dsun:dsun123@cluster0.s5zv6nz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const PORT = process.env.PORT || 5000;
 
+// use mongoose to connect to our database
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
-
-// mongoose.set('useFindAndModify', false);
