7d5ec9e71d531694c0cbbf35a66541316a6d050d
Rpolitis <Ryan_Politis@student.uml.edu>
2024-03-17 15:25:12 -0400

    v0.0.9

--- client/src/pages/Login/login.js
+++ client/src/pages/Login/login.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Input from "./input";
+import './login.css';
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { Grid, Button } from "@mui/material";
+import { Grid, Button, Paper, Container } from "@mui/material";
 import { signup, login } from "../../actions/auth";
 
 const initState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: ''}
@@ -31,38 +32,40 @@ const Login = () => {
     };
 
     return (
-        <div>
-            <h1>Hello login!</h1>
-            <form className="form" onSubmit={handleSubmit}>
-                <Grid container spacing={2}>
-                    {
-                        isSignup && (
-                            <>
-                                <Input name="firstName" label="First Name" handleChange={handleChange} half />
-                                <Input name="lastName" label="Last Name" handleChange={handleChange} half />
-                            </>
-                        )
-                    }
-                    <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
-                    <Input name="password" label="Password" handleChange={handleChange} type="password" />
-                    { isSignup && <Input name="confirmPassword" label="Retype Password" handleChange={handleChange} type="password" />}
-                </Grid>
-                <Button type="Submit" fullWidth className="Submit">
-                    {
-                        isSignup ? 'Sign Up' : 'Log in'
-                    }
-                </Button>
-                <Grid container justify="flex-end">
-                    <Grid item>
-                        <Button onClick={switchMode}>
-                            {
-                                isSignup ? 'Already have an account? Log in' : "Don't have an account? Sign up"
-                            }
-                        </Button>
+        <Container component="main" maxWidth="xs" className="main">
+            <Paper elevation={3} className="paper">
+                <h1>welcome to texturetalk.</h1>
+                <form className="form" onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+                                <>
+                                    <Input name="firstName" label="First Name" handleChange={handleChange} half />
+                                    <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+                                </>
+                            )
+                        }
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                        <Input name="password" label="Password" handleChange={handleChange} type="password" />
+                        { isSignup && <Input name="confirmPassword" label="Retype Password" handleChange={handleChange} type="password" />}
                     </Grid>
-                </Grid>
-            </form>
-        </div>
+                    <Button type="Submit" fullWidth className="Submit">
+                        {
+                            isSignup ? 'Sign Up' : 'Log in'
+                        }
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                {
+                                    isSignup ? 'Already have an account? Log in' : "Don't have an account? Sign up"
+                                }
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     );
 };
  
