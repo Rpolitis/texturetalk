c21698c835e078805c11cb3c85093ad2b8fb1808
aririb <arianapb03@gmail.com>
2024-03-17 22:39:20 -0400

    update1.1

--- client/src/pages/Quiz/1startquiz.js
+++ client/src/pages/Quiz/1startquiz.js
@@ -1,23 +1,30 @@
 import React from "react";
-// import { useHistory } from 'react-router-dom';
+import './button.css';
 
-// const StartQuiz = () => {
-//     const history = useHistory();
-
-//     const handleStartQuiz = () => {
-//         history.push('/2type.js'); // redirect to q1
-//     };
-// };
-
-const Quiz = () => {
+const StartQuiz = ({ onStartQuiz }) => {
     return (
         <div style={{ textAlign: "center", marginTop: "50px" }}>
             <h1>Ready to find your perfect hair routine?</h1>
-            <h3>Take our quick quiz for your personalized match.</h3>
-            {/* <button className="start-button" onClick={handleStartQuiz}>
-                start the quiz
-            </button> */}
+            <h2>Take our quick quiz for your personalized match.</h2>
+            <button className="start-button" onClick={onStartQuiz}>
+                Start the Quiz
+            </button>
         </div>
+    );
+};
+
+const Quiz = () => {
+    const handleStartQuiz = () => {
+        // Redirect to the quiz page
+        redirectToQuizPage(); // Assuming redirectToQuizPage is defined in the parent component
+    };
+
+    const redirectToQuizPage = () => {
+        window.location.href = '/3texture.js'; // Redirect to the quiz page
+    };
+
+    return (
+        <StartQuiz onStartQuiz={handleStartQuiz} />
     );
 };
  
