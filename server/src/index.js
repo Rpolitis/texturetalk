3f7a4a72579b634dfb38b518b4bca565951de932
Qster <colbymei2020@gmail.com>
2024-03-18 02:14:36 -0400

    samething 1.4

--- server/src/index.js
+++ server/src/index.js
@@ -1,56 +1,23 @@
-// import express from 'express';
-// import bodyParser from 'body-parser';
-// import mongoose from 'mongoose';
-// import cors from 'cors';
-
-// const express = require("express");
-// const app = express();
-// app.listen(3000,() => console.log("Server listening at port 3000"));
-
-// app.get("/", (req, res) => {
-//     res.send("Hello World")
-// })
-
-// server.js
-
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const authRoutes = require('./routes/authRoutes');
-import authRoutes from './routes/authRoutes.js'
-
-// import the router which we exported from posts.js
+import authRoutes from './routes/authRoutes.js';
 import postRoutes from './routes/posts.js';
-import userRouter from './routes/authRoutes.js';
 
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-// Middleware
-app.use(express.json());
-
-// use express middleware to connect this to our application
-// every route inside of postRoutes is going to start with /posts
+// Routes
 app.use('/posts', postRoutes);
-// Use auth routes
 app.use('/api/auth', authRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://dsun:dsun123@cluster0.s5zv6nz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const CONNECTION_URL = 'mongodb+srv://dsun:dsun123@cluster0.s5zv6nz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = process.env.PORT || 5000;
 
-// use mongoose to connect to our database
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
-
-// Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`);
-// });
+    .catch((error) => console.error(error.message));
