13976cdcf51ef7e797d5fef0da0ae2352bdbe630
aririb <arianapb03@gmail.com>
2024-03-18 01:39:14 -0400

    appjsedits

--- client/src/pages/Quiz/q1type.js
+++ client/src/pages/Quiz/q1type.js
@@ -2,11 +2,16 @@ import React from "react";
 
 const Q1Type = () => {
     return (
-        <div>
-            <h1>helllooooo q1</h1>
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>What is your natural hair type?</h1>
+            <div>
+                <button className="hair-type-button">Straight</button>
+                <button className="hair-type-button">Wavy</button>
+                <button className="hair-type-button">Curly</button>
+                <button className="hair-type-button">Coily</button>
+            </div>
         </div>
     );
 };
 
 export default Q1Type;
-
--- client/src/pages/Quiz/startquiz.js
+++ client/src/pages/Quiz/startquiz.js
@@ -1,33 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './button.css';
 
 // StartQuiz component represents the start page of the quiz
-const StartQuiz = ({ onStartQuiz }) => {
+const StartQuiz = () => {
     return (
         <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", minHeight: "75vh" }}>
             <div style={{ textAlign: "center" }}>
-                <h1>ready to find your perfect hair routine?</h1>
-                <h3>take our quick quiz for your personalized match.</h3>
-                {/* Button to start the quiz */}
-                <button className="start-button" onClick={onStartQuiz}>
-                    start the quiz
-                </button>
+                <h1>Ready to find your perfect hair routine?</h1>
+                <h3>Take our quick quiz for your personalized match.</h3>
+                {/* Link to the first question page */}
+                <Link to="/q1type" className="start-button">
+                    Start the Quiz
+                </Link>
             </div>
         </div>
     );
 };
 
-// Quiz component acts as the parent component for the start page of the quiz
-const Quiz = () => {
-    // Function to handle starting the quiz
-    const handleStartQuiz = () => {
-        // Redirect to the quiz page by changing the window location
-        window.location.href = '/q1type.js';
-    };
-
-    return (
-        <StartQuiz onStartQuiz={handleStartQuiz} />
-    );
-};
- 
-export default Quiz;
+export default StartQuiz;
