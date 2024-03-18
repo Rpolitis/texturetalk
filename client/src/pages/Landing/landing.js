545450ba96c26e692bfaa9bda10d0afc7115cb89
Rpolitis <Ryan_Politis@student.uml.edu>
2024-03-17 20:27:24 -0400

    v0.0.11

--- client/src/pages/Landing/landing.js
+++ client/src/pages/Landing/landing.js
@@ -3,9 +3,11 @@ import './landing.css';
 
 const Landing = () => {
     return (
+        <>
         <div className="site_title">
             <h1>texturetalk.</h1>
         </div>
+        </>
     );
 };
  
