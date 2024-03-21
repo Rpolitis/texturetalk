6bdb90029b7a242bffa8e57206d3bda244daa34e
Qster <colbymei2020@gmail.com>
2024-03-20 22:07:50 -0400

    env

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,30 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 import authRoutes from './routes/authRoutes.js';
 import postRoutes from './routes/posts.js';
 
 const app = express();
 
+const corsOptions = {
+    origin: "http://localhost:3000" // frontend URI (ReactJS)
+}
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Routes
 app.use('/posts', postRoutes);
 app.use('/api/auth', authRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://dsun:dsun123@cluster0.s5zv6nz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.error(error.message));
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    const PORT = process.env.PORT || 8000
+    app.listen(PORT, () => {
+        console.log(`App is Listening on PORT ${PORT}`);
+    })
+}).catch(err => {
+    console.log(err);
+});
